180cc5f4455d2cd3543205d172d9789bbf015000
MichaelCummings9715 <150955366+MichaelCummings9715@users.noreply.github.com>
2025-03-13 10:08:47 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,39 +3,39 @@ package school;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	   // TODO Auto-generated method stub
 		
-		Library library = Library.getInstance();
+	   Library library = Library.getInstance();
 
-        // Create some test books
-        Book book1 = new Book();
-        book1.title = "The Great Gatsby";
-        book1.author = "F. Scott Fitzgerald";
-        book1.publicationYear = 1925;
+           // Create some test books
+           Book book1 = new Book();
+           book1.title = "The Great Gatsby";
+           book1.author = "F. Scott Fitzgerald";
+           book1.publicationYear = 1925;
        
-        Book book2 = new Book();
-        book2.title = "1984";
-        book2.author = "George Orwell";
+           Book book2 = new Book();
+           book2.title = "1984";
+           book2.author = "George Orwell";
 
-        // create test magazine
-        Magazine mag1 = new Magazine();
-        mag1.title = "Time Magazine";
-        mag1.issueNumber = 123;
+           // create test magazine
+           Magazine mag1 = new Magazine();
+           mag1.title = "Time Magazine";
+           mag1.issueNumber = 123;
 
-        // test adding items to the library
-        library.addItem(book1);
-        library.addItem(book2);
-        library.addItem(mag1);
+           // test adding items to the library
+           library.addItem(book1);
+           library.addItem(book2);
+           library.addItem(mag1);
 
-        // Test findItemByTitle
-        System.out.println(library.findItemByTitle("the great gatsby"));
-        System.out.println(library.findItemByTitle("1984"));
+           // Test findItemByTitle
+           System.out.println(library.findItemByTitle("the great gatsby"));
+           System.out.println(library.findItemByTitle("1984"));
 
-	// Test item availability
-	library.listAvailableItems();
+	   // Test Library availability
+	   library.listAvailableItems();
 
-        // Test with a non-existent title
-        System.out.println(library.findItemByTitle("Harry Potter"));
+           // Test with a non-existent title
+           System.out.println(library.findItemByTitle("Harry Potter"));
 
 	}
 
